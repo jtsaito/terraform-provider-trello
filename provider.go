daf38e5188ae97ee4cec6f4ea6aaeabe090205f2
jtsaito <jahn.saito@gmail.com>
2019-02-02 19:04:18 +0100

    Add provider with dummy CRUD operations

--- provider.go
+++ provider.go
@@ -6,6 +6,7 @@ import "github.com/hashicorp/terraform/helper/schema"
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
+			"trello_board": resourceBoard(),
 		},
 		// ConfigureFunc: providerConfigure,
 	}
