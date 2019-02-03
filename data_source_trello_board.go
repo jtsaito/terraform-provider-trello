df3b8f029b18b06cb04ae615bc67c7faf8db6404
jtsaito <jahn.saito@gmail.com>
2019-02-03 21:16:54 +0100

    Add implementation of read callback to board

--- data_source_trello_board.go
+++ data_source_trello_board.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
 
 	"github.com/adlio/trello"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -27,5 +25,28 @@ func dataSourceTrelloBoard() *schema.Resource {
 }
 
 func dataSourceTrelloBoardRead(d *schema.ResourceData, m interface{}) error {
+	member := m.(*TrelloConfig).Member
+
+	boards, err := member.GetBoards(trello.Defaults())
+	if err != nil {
+		return err
+	}
+
+	var board *trello.Board
+	for _, b := range boards {
+		if b.Name == d.Get("name") {
+			board = b
+			break
+		}
+	}
+
+	if board == nil {
+		return fmt.Errorf("not found. board %s", d.Get("name"))
+	}
+
+	d.SetId(board.ID)
+	d.Set("name", board.Name)
+	d.Set("description", board.Desc)
+
 	return nil
 }
